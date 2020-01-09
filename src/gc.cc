01e36138cc4b41d42f6417b35888a6fcc8efd62a
wy3 <wuuyi123@gmail.com>
2020-01-09 16:04:35 +0700

    add GC

---
+++ src/gc.cc
@@ -0,0 +1,18 @@
+#include "gc.hpp"
+
+using namespace aup;
+
+GC::GC()
+{
+
+}
+
+GC::~GC()
+{
+
+}
+
+void *GC::realloc(void *ptr, size_t oldSize, size_t newSize)
+{
+
+}
---
+++ src/gc.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <cstddef>
+
+namespace aup
+{
+    struct GC
+    {
+        size_t allocated;
+        size_t nextGC;
+
+        GC();
+        ~GC();
+        void *realloc(void *ptr, size_t oldSize, size_t newSize);
+    };
+}
